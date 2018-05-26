7728e03570324700cdf3112f817acc9557701985
Jasmine <jasmine163lover@163.com>
2018-05-26 15:54:43 +0800

    字体图标换

--- src/components/details/details.vue
+++ src/components/details/details.vue
@@ -1,8 +1,40 @@
 <template>
-  <div>
-     <h1>首页</h1>
-  </div>
+    <div>
+        <ul class="x_header">
+            <li class="back"><i class="icon iconfont icon-back"></i></li>
+            <li class="stores">商品</li>
+            <li class="comment">评价</li>
+            <li class="info">详情</li>
+            <li class="recommend">推荐</li>
+        </ul>
+        <div class="x_main">
+            <div class="nav"></div>
+            <div class="x_banner">
+            
+            </div>
+            <div class="x_msg">
+                <div class="name"></div>
+                <div class="sell"></div>
+                <div class="price"></div>
+                <div class="ad"></div>
+            </div>
+        </div>        
+        <div class="x_footer">
+            <div class="left">
+                <div class="collect"><i class="icon iconfont icon-favorite"></i><p>收藏</p></div>
+                <div class="stores"><p>小店</p></div>
+                <div class="cart"><i class="icon iconfont icon-cart"></i><p>购物车</p></div>
+            </div>
+            <div class="addCart"></div>
+            <div class="buy"></div>
+        </div>
+    </div>
 </template>
 <script>
-    export default{}
+    import './details.css'
+    export default{
+        data(){
+            return {}
+        }
+    }
 </script>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-     <h1>首页</h1>
+    <div>
+        <h1>SIGN IN</h1>
+    </div>
+     
+     <ul>
+         <li><input type="text" placeholder="Username"/></li>
+         <li><input type="text" placeholder="Password"/></li>
+     </ul>
+     <ul>
+         <li>Login</li>
+         <li>Register</li>
+     </ul>
   </div>
 </template>
 <script>
