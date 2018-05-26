d0a73318bbc15394f9ed9b1f5d9e18660e4ee46e
Jasmine <jasmine163lover@163.com>
2018-05-26 16:21:58 +0800

    index图标

--- src/components/details/details.vue
+++ src/components/details/details.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <ul class="x_header">
-            <li class="back"><i class="icon iconfont icon-back"></i></li>
+            <li class="back"><i class="fa fa-chevron-left" aria-hidden="true"></i></li>
             <li class="stores">商品</li>
             <li class="comment">评价</li>
             <li class="info">详情</li>
             <li class="recommend">推荐</li>
+            <li class="back_home"><i class="fa fa-home" aria-hidden="true"></i></li>
         </ul>
         <div class="x_main">
             <div class="nav"></div>
@@ -23,7 +24,7 @@
             <div class="left">
                 <div class="collect"><i class="icon iconfont icon-favorite"></i><p>收藏</p></div>
                 <div class="stores"><p>小店</p></div>
-                <div class="cart"><i class="icon iconfont icon-cart"></i><p>购物车</p></div>
+                <div class="cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i><p>购物车</p></div>
             </div>
             <div class="addCart"></div>
             <div class="buy"></div>
